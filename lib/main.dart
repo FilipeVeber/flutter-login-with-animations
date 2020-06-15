868ef3f98869fa6693090a745cd4e34fdbdb1d77
Filipe Veber <filipe.veber@supero.com.br>
2020-06-15 17:52:58 -0300

    WIP: Login screen and input fields

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'modules/login_module/login_screen.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return MaterialApp(
+        title: "Login with animations",
+        debugShowCheckedModeBanner: false,
+        home: LoginScreen());
   }
 }
